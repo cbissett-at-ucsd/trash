cb5246b99b3cca3ea230c27be4f52c7332515549
cbissett-at-ucsd <cbissett@ucsd.edu>
2021-06-05 16:52:08 -0700

    fin

--- web/src/server.py
+++ web/src/server.py
@@ -1,17 +1,46 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+from pyramid.renderers import render_to_response
+
+import requests
+import os
+import json
+
+
 
 
 def hello_world(request):
-    return Response('Hello World!')
+    return Response("hello")
+
+def double(req):
+    page_data = req.GET.mixed()
+    num=int(page_data['id'])
+    print(num)
+    num=num*2
+    print(num)
+    return Response(str(num))
+
+def get_form(req):
+    return render_to_response('form.html', {}, request=req)
 
 
 if __name__ == '__main__':
     print("connected")
-    with Configurator() as config:
-        config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='hello')
-        app = config.make_wsgi_app()
+    config = Configurator()
+    config.include('pyramid_jinja2')
+    config.add_jinja2_renderer('.html')
+
+    config.add_route('hello', '/')
+    config.add_view(hello_world, route_name='hello')
+
+    config.add_route('form','/form')
+    config.add_view(get_form,route_name='form')
+
+    config.add_route('double', '/double')
+    config.add_view(double, route_name='double', request_method='GET')
+
+
+    app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 6543, app)
     server.serve_forever()
