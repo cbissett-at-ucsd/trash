0a68f08857824a259a8a81afebb57b0f7e7d150a
cbissett-at-ucsd <cbissett@ucsd.edu>
2021-06-05 15:02:41 -0700

    hk

--- web/src/server.py
+++ web/src/server.py
@@ -6,24 +6,12 @@ from pyramid.response import Response
 def hello_world(request):
     return Response('Hello World!')
 
-def double(request):
-    x=request.get('int')
-    x=x*2
-    return Response(x)
-
 
 if __name__ == '__main__':
+    print("connected")
     with Configurator() as config:
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello')
-
-        config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='hello')
-
-        config.add_route('double', '/double')
-        config.add_view(double, route_name = 'double', request_method='GET')
-
-
         app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', 6666, app)
+    server = make_server('0.0.0.0', 6543, app)
     server.serve_forever()
